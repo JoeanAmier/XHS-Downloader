5b5db2c3edda5b64e569dd5f45874a49cd7ad2c4
JoeamAmier <yonglelolu@gmail.com>
2023-09-17 10:49:21 +0800

    优化链接提取逻辑

--- source/Image.py
+++ source/Image.py
@@ -6,7 +6,7 @@ __all__ = ['Image']
 
 
 class Image:
-    IMAGE_URL = compile(r'"CRD_WM_[A-Z]{3,4}","url":"(.*?)"')
+    IMAGE_URL = compile(r'"CRD_WM_[A-Z]{3,4}","url":"(.*?_wm_1)"')
 
     def get_image_link(self, html: str) -> list:
         return [Html.format_url(i) for i in self.IMAGE_URL.findall(html)]
