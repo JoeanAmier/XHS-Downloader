559d95aff14364872b229742bf1adcb5137b31ad
Elllllllvin <100711005+Elllllllvin@users.noreply.github.com>
2023-12-31 09:49:47 +0800

    现有代码存在一些问题，修改之后可以适配3.10版本的python
    修复了一些运行上可能出现的问题

---
+++ Video.py
@@ -0,0 +1,12 @@
+from re import compile
+
+from .Html import Html
+
+__all__ = ['Video']
+
+
+class Video:
+    VIDEO_TOKEN = compile(r'"originVideoKey":"(\S+?)"')
+
+    def get_video_link(self, html: str) -> list:
+        return [Html.format_url(f"https://sns-video-hw.xhscdn.com/{t.group(1)}")] if (t := self.VIDEO_TOKEN.search(html)) else []
